a812bf8dab30d5eab54df6b0992eea7d633aa6aa
KimJongJo <desk1614@gmail.com>
2025-12-03 12:06:06 +0900

    import, 관리자 경로 설정

--- zipddak_front/src/App.jsx
+++ zipddak_front/src/App.jsx
@@ -43,77 +43,147 @@ import ToolBorrow from "./user/myPage/ToolBorrow";
 import ToolBorrowDetail from "./user/myPage/ToolBorrowDetail";
 import ToolLent from "./user/myPage/ToolLent";
 import ToolLentDetail from "./user/myPage/ToolLentDetail";
+import ProductList from "./user/product/ProductList";
+import ProductDetail from "./user/product/ProductDetail";
+import ProductOrder from "./user/product/ProductOrder";
+import ProductOrderComplate from "./user/product/ProductOrderComplate";
+import StoreInfo from "./user/product/StoreInfo";
+import Cart from "./user/product/Cart";
+import Experts from "./user/expert/Experts";
+import ExpertProfile from "./user/expert/ExpertProfile";
+import ExpertMatchPayment from "./user/expert/ExpertMatchPayment";
+import FindExpert from "./user/expert/FindExpert";
+import AdminUserList from "./admin/pages/AdminUserList";
+import AdminExpertList from "./admin/pages/AdminExpertList";
+import AdminStoreList from "./admin/pages/AdminStoreList";
+import AdminUserDetail from "./admin/pages/AdminUserDetail";
+import AdminExpertDetail from "./admin/pages/AdminExpertDetail";
+import AdminStoreDetail from "./admin/pages/AdminStoreDetail";
+import AdminSwitchAccountRequests from "./admin/pages/AdminSwitchAccountRequests";
+import AdminRentalRecords from "./admin/pages/AdminRentalRecords";
+import AdminRentalDetail from "./admin/pages/AdminRentalDetail";
+import AdminSalesHistory from "./admin/pages/AdminSalesHistory";
+import AdminSalesDetail from "./admin/pages/AdminSalesDetail";
+import AdminReportList from "./admin/pages/AdminReportList";
+import AdminReportDetail from "./admin/pages/AdminReportDetail";
+import AdminMatchingDetail from "./admin/pages/AdminMatchingDetail";
+import PaymentHistory from "./admin/pages/PaymentHistory";
+import AdminPaymentDetail from "./admin/pages/AdminPaymentDetail";
+import AdminMembership from "./admin/pages/AdminMembership";
+import AdminSettlementList from "./admin/pages/AdminSettlementList";
+import InquiryList from "./admin/pages/InquiryList";
+import InquiryReturn from "./admin/pages/InquiryReturn";
+import Dashboard from "./admin/pages/Dashboard";
 
 function App() {
-  return (
-    <Routes>
-      {/* 일반사용자 로그인 */}
-      <Route path="login" element={<Login/>}/>
-      <Route path="signUp/user" element={<SignUser/>}/>
-      <Route path="signUp/expert" element={<SignExpert/>}/>
-      <Route path="signUp/store1" element={<SignStore1/>}/>
-      <Route path="signUp/store2" element={<SignStore2/>}/>
-      <Route path="signUp/store3" element={<SignStore3/>}/>
+    return (
+        <Routes>
+            {/* 일반사용자 로그인 */}
+            <Route path="login" element={<Login />} />
+            <Route path="signUp/user" element={<SignUser />} />
+            <Route path="signUp/expert" element={<SignExpert />} />
+            <Route path="signUp/store1" element={<SignStore1 />} />
+            <Route path="signUp/store2" element={<SignStore2 />} />
+            <Route path="signUp/store3" element={<SignStore3 />} />
 
+            {/* 일반사용자 */}
+            <Route path="/" element={<Main />} />
+            <Route path="/search" element={<MainSearch />} />
+            <Route path="market/return/:orderId" element={<MarketReturnForm />} />
 
-      {/* 일반사용자 */}
-      <Route path="/" element={<Main/>}/>
-      <Route path="/search" element={<MainSearch/>}/>
-      <Route path="market/return/:orderId" element={<MarketReturnForm />} />
+            {/* 일반사용자 마이페이지 */}
+            <Route path="/user/mypage/*" element={<Mypage />}>
+                <Route path="account" element={<Account />} />
+                <Route path="expert/works" element={<MyWorks />} />
+                <Route path="inquiries" element={<Inquiries />} />
+                <Route path="inquiries/write" element={<InquiryForm />} />
+                <Route path="community" element={<Community />} />
+                <Route path="likes" element={<Likes />} />
+                <Route path="reviews" element={<Reviews />} />
+                <Route path="market/orders" element={<MarketOrders />} />
+                <Route path="market/returns" element={<MarketReturns />} />
+                <Route path="market/detail/:orderId" element={<MarketOrderDetail />} />
+                <Route path="market/exchange/:orderId" element={<MarketExchangeForm />} />
+                <Route path="tool" element={<MyTool />} />
+                <Route path="tool/borrow" element={<ToolBorrow />} />
+                <Route path="tool/borrow/:rentalId" element={<ToolBorrowDetail />} />
+                <Route path="tool/lent" element={<ToolLent />} />
+                <Route path="tool/lent/:rentalId" element={<ToolLentDetail />} />
+            </Route>
 
-      
+            {/* 일반사용자 공구대여 */}
+            <Route path="tool" element={<ToolMain />} />
+            <Route path="tool/:toolId" element={<ToolDetail />} />
+            <Route path="tool/regist" element={<RegistTool />} />
+            <Route path="tool/apply" element={<ApplyTool />} />
 
-      {/* 일반사용자 마이페이지 */}
-      <Route path="/user/mypage/*" element={<Mypage />}>
-        <Route path="account" element={<Account />} />
-        <Route path="expert/works" element={<MyWorks />} />
-        <Route path="inquiries" element={<Inquiries />} />
-        <Route path="inquiries/wrtie" element={<InquiryForm />} />
-        <Route path="community" element={<Community />} />
-        <Route path="likes" element={<Likes />} />
-        <Route path="reviews" element={<Reviews />} />
-        <Route path="market/orders" element={<MarketOrders />} />
-        <Route path="market/returns" element={<MarketReturns />} />
-        <Route path="market/detail/:orderId" element={<MarketOrderDetail />} />
-        <Route
-          path="market/exchange/:orderId"
-          element={<MarketExchangeForm />}
-        />
-        <Route path="tool" element={<MyTool/>}/>
-        <Route path="tool/borrow" element={<ToolBorrow/>}/>
-        <Route path="tool/borrow/:rentalId" element={<ToolBorrowDetail/>}/>
-        <Route path="tool/lent" element={<ToolLent/>}/>
-        <Route path="tool/lent/:rentalId" element={<ToolLentDetail/>}/>
-      </Route>
+            {/* 일반사용자 커뮤니티 */}
+            <Route path="community" element={<CommunityList />} />
+            <Route path="community/write" element={<ComForm />} />
+            <Route path="community/:communityId" element={<Comdetail />} />
 
-      {/* 일반사용자 공구대여 */}
-      <Route path="tool" element={<ToolMain/>}/>
-      <Route path="tool/:toolId" element={<ToolDetail/>}/>
-      <Route path="tool/regist" element={<RegistTool/>}/>
-      <Route path="tool/apply" element={<ApplyTool/>}/>
+            {/* 일반사용자 자재구매 */}
+            <Route path="productList" element={<ProductList />} />
+            <Route path="product/:productId" element={<ProductDetail />} />
+            <Route path="productOrder" element={<ProductOrder />} />
+            <Route path="productOrderComplate" element={<ProductOrderComplate />} />
+            <Route path="storeInfo" element={<StoreInfo />} />
 
-      {/* 일반사용자 커뮤니티 */}
-      <Route path="community" element={<CommunityList/>}/>
-      <Route path="community/write" element={<ComForm/>}/>
-      <Route path="community/:communityId" element={<Comdetail/>}/>
+            {/* 일반사용자 장바구니 */}
+            <Route path="cart" element={<Cart />} />
 
-      {/* 전문가 */}
-      <Route path="/expert/profile/edit" element={<ProfileForm />} />
-      <Route path="/expert/requests" element={<PublicRequest />} />
+            {/* 일반사용자 전문가찾기 */}
+            <Route path="experts" element={<Experts />} />
+            <Route path="expertProfile:/username" element={<ExpertProfile />} />
+            <Route path="expertMatchPayment" element={<ExpertMatchPayment />} />
+            <Route path="findExpert" element={<FindExpert />} />
 
-      {/* 전문가 마이페이지 */}
-      <Route path="/expert/mypage/*" element={<ExpertMypage />}>
-        <Route path="works" element={<ExpertMyWorks />} />
-        <Route path="requests" element={<Requests />} />
-        <Route path="inquiries" element={<ExpertInquiries />} />
-        <Route path="inquiries/wrtie" element={<ExpertInquiryForm />} />
-        <Route path="community" element={<ExpertCommunity />} />
-        <Route path="account" element={<ExpertAccount />} />
-        <Route path="settlement" element={<Settlement />} />
-        <Route path="membership" element={<Membership />} />
-      </Route>
-    </Routes>
-  );
+            {/* 전문가 */}
+            <Route path="/expert/profile/edit" element={<ProfileForm />} />
+            <Route path="/expert/requests" element={<PublicRequest />} />
+
+            {/* 전문가 마이페이지 */}
+            <Route path="/expert/mypage/*" element={<ExpertMypage />}>
+                <Route path="works" element={<ExpertMyWorks />} />
+                <Route path="requests" element={<Requests />} />
+                <Route path="inquiries" element={<ExpertInquiries />} />
+                <Route path="inquiries/wrtie" element={<ExpertInquiryForm />} />
+                <Route path="community" element={<ExpertCommunity />} />
+                <Route path="account" element={<ExpertAccount />} />
+                <Route path="settlement" element={<Settlement />} />
+                <Route path="membership" element={<Membership />} />
+            </Route>
+
+            {/* 사이트 관리자 */}
+            <Route path="admin/*">
+                <Route path="userList" element={<AdminUserList />} />
+                <Route path="expertList" element={<AdminExpertList />} />
+                <Route path="storeList" element={<AdminStoreList />} />
+                <Route path="userList/:username" element={<AdminUserDetail />} />
+                <Route path="userList/:username" element={<AdminExpertDetail />} />
+                <Route path="userList:/storeId" element={<AdminStoreDetail />} />
+                <Route path="switchRequest" element={<AdminSwitchAccountRequests />} />
+
+                <Route path="rentalList" element={<AdminRentalRecords />} />
+                <Route path="rentalList/:rentalId" element={<AdminRentalDetail />} />
+                <Route path="salesList" element={<AdminSalesHistory />} />
+                <Route path="salesList/:saleId" element={<AdminSalesDetail />} />
+                <Route path="matching/:matchingId" element={<AdminMatchingDetail />} />
+                <Route path="reports" element={<AdminReportList />} />
+                <Route path="reports/:reportId" element={<AdminReportDetail />} />
+                <Route path="payments" element={<PaymentHistory />} />
+                <Route path="payments/:paymentId" element={<AdminPaymentDetail />} />
+                <Route path="membership" element={<AdminMembership />} />
+
+                <Route path="settlementList" element={<AdminSettlementList />} />
+
+                <Route path="inquiryList" element={<InquiryList />} />
+                <Route path="inquiryList:/inquiryId" element={<InquiryReturn />} />
+
+                <Route path="dashboard" element={<Dashboard />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- zipddak_front/src/user/login/SignUser.jsx
+++ zipddak_front/src/user/login/SignUser.jsx
@@ -1,5 +1,5 @@
-import { Form, FormGroup, Label, Input, Button, Col } from 'reactstrap'
-import './css/Signup.css'
+import { Form, FormGroup, Label, Input, Button, Col } from "reactstrap";
+import "../css/Signup.css";
 
 export default function SignUser() {
     return (
@@ -8,180 +8,111 @@ export default function SignUser() {
                 <div className="logo"></div>
                 <div className="title">회원가입</div>
 
-                <div className='sns_login'>
-                    <div className='sns_top'>
-
-                        <div className='sns_title'>sns계정으로 간편 회원가입</div>
-
+                <div className="sns_login">
+                    <div className="sns_top">
+                        <div className="sns_title">sns계정으로 간편 회원가입</div>
                     </div>
-                    <div className='sns_icon'>
+                    <div className="sns_icon">
                         <img src="naver_r.png" alt="" style={{ width: 56, height: 56 }} />
                         <img src="kakao_r.png" alt="" style={{ width: 56, height: 56 }} />
                         <img src="google_r.png" alt="" style={{ width: 56, height: 56 }} />
                     </div>
                 </div>
 
-                <div className='line1'></div>
+                <div className="line1"></div>
 
                 <Form>
-                    <div className='input_form'>
-                        <div className='input_parts'>
-                            <div className='input_label'>이름</div>
-                            <Input
-                                name="name"
-                                placeholder="이름(실명)을 입력해주세요"
-                                type="text"
-                            />
+                    <div className="input_form">
+                        <div className="input_parts">
+                            <div className="input_label">이름</div>
+                            <Input name="name" placeholder="이름(실명)을 입력해주세요" type="text" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>이메일</div>
-                            <Input
-                                id="email"
-                                name="email"
-                                placeholder="아이디로 사용할 이메일을 입력해주세요."
-                                type="email"
-                            />
+                        <div className="input_parts">
+                            <div className="input_label">이메일</div>
+                            <Input id="email" name="email" placeholder="아이디로 사용할 이메일을 입력해주세요." type="email" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>비밀번호</div>
-                            <div className='input_detail'>영문, 숫자를 포함한 8자 이상의 비밀번호를 설정해주세요</div>
+                        <div className="input_parts">
+                            <div className="input_label">비밀번호</div>
+                            <div className="input_detail">영문, 숫자를 포함한 8자 이상의 비밀번호를 설정해주세요</div>
 
-                            <Input
-                                id="password"
-                                name="password"
-                                placeholder="비밀번호를 입력해주세요."
-                                type="password"
-                            />
+                            <Input id="password" name="password" placeholder="비밀번호를 입력해주세요." type="password" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>비밀번호 확인</div>
-                            <Input
-                                id="cPassword"
-                                name="cPassword"
-                                placeholder="비밀번호를 입력해주세요."
-                                type="password"
-                            />
+                        <div className="input_parts">
+                            <div className="input_label">비밀번호 확인</div>
+                            <Input id="cPassword" name="cPassword" placeholder="비밀번호를 입력해주세요." type="password" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>닉네임</div>
-                            <Input
-                                id="nickname"
-                                name="nickname"
-                                placeholder="닉네임(2~20자)"
-                                type="text"
-                            />
+                        <div className="input_parts">
+                            <div className="input_label">닉네임</div>
+                            <Input id="nickname" name="nickname" placeholder="닉네임(2~20자)" type="text" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>휴대폰 인증</div>
-                            <Input
-                                id="tel"
-                                name="tel"
-                                placeholder="'-'없이 숫자만 입력"
-                                type="text"
-                            >
+                        <div className="input_parts">
+                            <div className="input_label">휴대폰 인증</div>
+                            <Input id="tel" name="tel" placeholder="'-'없이 숫자만 입력" type="text">
                                 <Button>전송</Button>
                             </Input>
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>주소</div>
-                            <div className='input_post'>
-                                <Input
-                                    name="zone"
-                                    placeholder="우편번호"
-                                    type="text"
-                                />
+                        <div className="input_parts">
+                            <div className="input_label">주소</div>
+                            <div className="input_post">
+                                <Input name="zone" placeholder="우편번호" type="text" />
                                 <Button>주소찾기</Button>
                             </div>
-                            <Input
-                                name="address"
-                                placeholder="도로명/지번 주소"
-                                type="text"
-                            />
-                            <Input
-                                name="detailAddress"
-                                placeholder="상세주소"
-                                type="text"
-                            />
+                            <Input name="address" placeholder="도로명/지번 주소" type="text" />
+                            <Input name="detailAddress" placeholder="상세주소" type="text" />
                         </div>
 
-                        <div className='input_parts'>
-                            <div className='input_label'>약관동의</div>
-                            <div className='input_detail'>
-                                회원가입 및 회원 관리등의 목적으로 이메일, 비밀번호, 휴대폰 번호 등의 정보를 수집 및 이용 하고 있습니다.
-                            </div>
-                            <div className='condition_box'>
-                                <div className='conditionAll'>
+                        <div className="input_parts">
+                            <div className="input_label">약관동의</div>
+                            <div className="input_detail">회원가입 및 회원 관리등의 목적으로 이메일, 비밀번호, 휴대폰 번호 등의 정보를 수집 및 이용 하고 있습니다.</div>
+                            <div className="condition_box">
+                                <div className="conditionAll">
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check className='check_All'>
+                                        <Input type="checkbox" />{" "}
+                                        <Label check className="check_All">
                                             전체동의
                                         </Label>
                                     </FormGroup>
                                 </div>
-                                <div className='line'></div>
-                                <div className='conditionOption'>
+                                <div className="line"></div>
+                                <div className="conditionOption">
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check>
-                                            이용약관(필수)
-                                        </Label>
+                                        <Input type="checkbox" /> <Label check>이용약관(필수)</Label>
                                     </FormGroup>
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check>
-                                            만 14세 이상입니다(필수)
-                                        </Label>
+                                        <Input type="checkbox" /> <Label check>만 14세 이상입니다(필수)</Label>
                                     </FormGroup>
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check>
-                                            개인정보 수집 및 이용동의(선택)
-                                        </Label>
+                                        <Input type="checkbox" /> <Label check>개인정보 수집 및 이용동의(선택)</Label>
                                     </FormGroup>
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check>
-                                            개인정보 마케팅 활용동의(선택)
-                                        </Label>
+                                        <Input type="checkbox" /> <Label check>개인정보 마케팅 활용동의(선택)</Label>
                                     </FormGroup>
                                     <FormGroup check>
-                                        <Input type="checkbox" />
-                                        {' '}
-                                        <Label check>
-                                            이벤트, 쿠폰, 특가알림 메일 및 sms수신 (선택)
-                                        </Label>
+                                        <Input type="checkbox" /> <Label check>이벤트, 쿠폰, 특가알림 메일 및 sms수신 (선택)</Label>
                                     </FormGroup>
                                 </div>
                             </div>
                         </div>
 
-                        <div className='mainButton'>
+                        <div className="mainButton">
                             <Button>회원가입하기</Button>
                         </div>
-
                     </div>
-                </Form >
-
+                </Form>
 
                 <div className="loginFooter">
-                    <div className='input_detail'>
-                        이미 아이디가 있으신가요?
-                    </div>
-                    <a><div className='input_detail2'>로그인</div></a>
+                    <div className="input_detail">이미 아이디가 있으신가요?</div>
+                    <a>
+                        <div className="input_detail2">로그인</div>
+                    </a>
                 </div>
-
-            </div >
+            </div>
         </>
-    )
+    );
 }
